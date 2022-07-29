ad63d3d468be8fcd2e023da9a5ae16ad8f9b810a
ac-89 <alancarroll89@gmail.com>
2022-07-29 15:46:53 +0100

    Complete basic addition functionality.

--- script.js
+++ script.js
@@ -1,46 +1,80 @@
-let buttons = document.querySelectorAll("button");
-let display = document.querySelector(`.screen`);
+const calculator = document.querySelector(`.calc-body`);
+const numButtons = document.querySelectorAll("button");
+const display = document.querySelector(`.screen`);
 
-console.log(buttons);
-console.log(display);
+const calculate = (n1, operator, n2) => {
+  let result = "";
+  if (operator === "plus") {
+    result = parseFloat(n1) + parseFloat(n2);
+  } else if (operator === "minus") {
+    result = parseFloat(n1) - parseFloat(n2);
+  } else if (operator === "multiply") {
+    result = parseFloat(n1) * parseFloat(n2);
+  } else if (operator === "divide") {
+    result = parseFloat(n1) / parseFloat(n2);
+  }
 
-buttons.forEach((btn) => {
+  return result;
+};
+
+numButtons.forEach((btn) => {
   btn.addEventListener("click", function (e) {
-    console.log(e);
-    display.textContent += e.target.innerHTML;
+    const key = e.target;
+    const action = key.dataset.action;
+    const previousKeyType = calculator.dataset.previousKeyType;
+    const displayedNum = display.textContent;
+    const value = e.target.textContent;
+    if (!action) {
+      if (displayedNum === `0` || previousKeyType === `operator`) {
+        display.textContent = value;
+      } else {
+        display.textContent = displayedNum + value;
+      }
+      calculator.dataset.previousKeyType = `number`;
+    }
+    if (action === "clear") {
+      calculator.dataset.previousKeyType = `clear`;
+      display.textContent = "0";
+    }
+    if (action === "decimal") {
+      if (!displayedNum.includes(`.`)) {
+        display.textContent = displayedNum + `.`;
+      } else if (previousKeyType === `decimal`) {
+        display.textContent = `0.`;
+      }
+      calculator.dataset.previousKeyType = `decimal`;
+    }
+
+    if (
+      action === "plus" ||
+      action === "minus" ||
+      action === "multiply" ||
+      action === "divide"
+    ) {
+      const firstValue = calculator.dataset.firstValue;
+      const operator = calculator.dataset.operator;
+      const secondValue = displayedNum;
+
+      if (firstValue && operator && previousKeyType !== `operator`) {
+        const calcValue = calculate(firstValue, operator, secondValue);
+        display.textContent = calcValue;
+        calculator.dataset.firstValue = calcValue;
+      } else {
+        calculator.dataset.firstValue = displayedNum;
+      }
+      calculator.dataset.previousKeyType = `operator`;
+      calculator.dataset.operator = action;
+    }
+    if (action === `equals`) {
+      calculator.dataset.previousKeyType = `equals`;
+      const firstValue = calculator.dataset.firstValue;
+      const operator = calculator.dataset.operator;
+      const secondValue = displayedNum;
+
+      //   calculator.dataset.secondValue = displayedNum;
+      //   console.log(firstValue + secondValue);
+      console.log(firstValue, operator, secondValue);
+      display.textContent = calculate(firstValue, operator, secondValue);
+    }
   });
 });
-
-function add(a, b) {
-  return a + b;
-}
-
-function subtract(a, b) {
-  return a - b;
-}
-
-function multiply(a, b) {
-  return a * b;
-}
-
-function divide(a, b) {
-  return a / b;
-}
-
-function operate(a, o, b) {
-  if (o == "+") {
-    add(a, b);
-  } else if (o == "-") {
-    subtract(a, b);
-  } else if (o == "*") {
-    multiply(a, b);
-  } else if (o == "/") {
-    divide(a, b);
-  }
-}
-
-// function screen{
-
-// }
-
-// activateBtns();
