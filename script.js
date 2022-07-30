4e69fb33bd5f1791d325a2111dd8da007ef57706
ac-89 <alancarroll89@gmail.com>
2022-07-30 13:46:25 +0100

    Create CreateResultString function

--- script.js
+++ script.js
@@ -11,6 +11,57 @@ const calculate = (n1, operator, n2) => {
   if (operator === "divide") return firstNum / secondNum;
 };
 
+const createResultString = () => {
+  //Variables
+  //1. keyContent
+  //2. displayedNum
+  //3. previousKeyType
+  //4. action
+  //5. calculator.dataset.firstValue
+  //6. calculator.dataset.operator
+  if (!action) {
+    return displayedNum === `0` || previousKeyType === `operator`
+      ? value
+      : displayedNum + value;
+  }
+  if (action === "decimal") {
+    if (!displayedNum.includes(`.`)) return displayedNum + `.`;
+    if (previousKeyType === `operator` || previousKeyType === `calculate`)
+      return `0.`;
+    return displayedNum;
+  }
+  calculator.dataset.previousKeyType = `decimal`;
+  if (
+    action === "plus" ||
+    action === "minus" ||
+    action === "multiply" ||
+    action === "divide"
+  ) {
+    const firstValue = calculator.dataset.firstValue;
+    const operator = calculator.dataset.operator;
+    return firstValue &&
+      operator &&
+      previousKeyType !== `operator` &&
+      previousKeyType !== `calculate`
+      ? calculate(firstValue, operator, displayedNum)
+      : displayedNum;
+  }
+  if (action === "clear") return 0;
+
+  if (action === "all-clear") return 0;
+
+  if (action === `equals`) {
+    let firstValue = calculator.dataset.firstValue;
+    const operator = calculator.dataset.operator;
+    let secondValue = displayedNum;
+    return firstValue
+      ? previousKeyType === `equals`
+        ? calculate(displayedNum, operator, modValue)
+        : calculate(firstValue, operator, displayedNum)
+      : displayedNum;
+  }
+};
+
 numButtons.forEach((btn) => {
   btn.addEventListener("click", function (e) {
     const key = e.target;
